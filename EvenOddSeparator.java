da488ed4993a86602b86bb8bdafc2191f135af52
Mr-Thop <karthikreddy0314@gmail.com>
2025-02-03 18:31:38 +0530

    Creating Method processNumbers

--- EvenOddSeparator.java
+++ EvenOddSeparator.java
@@ -2,6 +2,25 @@
 import java.util.* ; 
 
 class EvenOddSeparator {
+    public static void processNumbers() {
+        Scanner scan = new Scanner(System.in);
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
+        
+        System.out.println("Enter numbers (type -1 to stop):");
+        while (true) {
+            int num = scan.nextInt();
+            if (num == -1) break;
+            if (num % 2 == 0) {
+                even.add(num);
+            } else {
+                odd.add(num);
+            }
+        }
+        
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+        scan.close();
+    }
 
-    
 }
