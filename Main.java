efd2bfa4e32d1d1731bb6c50f708518ea0cd42c2
Mr-Thop <karthikreddy0314@gmail.com>
2025-02-03 18:33:45 +0530

    Creating method main

--- Main.java
+++ Main.java
@@ -7,6 +7,24 @@
 import java.util.*;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Choose an operation:");
+        System.out.println("1. Separate even and odd numbers");
+        System.out.println("2. Find smallest distance pair");
+        System.out.println("3. Convert between Array and ArrayList");
+        System.out.println("4. Exit");
+        
+        int choice = scan.nextInt();
+        switch (choice) {
+            case 1 -> EvenOddSeparator.processNumbers();
+            case 2 -> SmallestDistanceFinder.findSmallestDistance();
+            case 3 -> ArrayConverter.convertArrayAndArrayList();
+            case 4 -> System.out.println("Exiting...");
+            default -> System.out.println("Invalid choice!");
+        }
+        scan.close();
+    }
 
 }
